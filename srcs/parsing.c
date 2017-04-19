41edf89acce32f0095b9425694aa92cff953ef33
Yohan BENOIT <ybenoit@e2r5p15.42.fr>
2017-04-19 19:10:57 +0200

    other parsing method

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: ybenoit <ybenoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 18:58:01 by ybenoit           #+#    #+#             */
-/*   Updated: 2017/04/19 19:06:47 by ybenoit          ###   ########.fr       */
+/*   Updated: 2017/04/19 19:10:42 by ybenoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 t_env			*init_env(t_env *e)
 {
-	char *line;
+	char line;
 
 	init_ant_n(e, line);
-	while ((get_next_line(0, &line) > 0))
+	if (ft_strstr(line, "##"))
 	{
-		if (ft_strstr(line, "##"))
-		{
-			if (ft_strstr(line, "start"))
-				init_start(e);
-			else if (ft_strstr(line, "line"))
-				init_end(e);
-		}
-		else
-			if (!ft_strchr(line, '-'))
-				init_room(e);
-			else
-				init_liaison(e);
+		if (ft_strstr(line, "start"))
+			init_start(e, line);
+		else if (ft_strstr(line, "line"))
+			init_end(e, line);
 	}
+	else
+		if (!ft_strchr(line, '-'))
+			init_room(e, line);
+		else
+		{
+			init_tab(e->tab);
+			init_liaison(e, line);
+		}
 	return (e);
 }
